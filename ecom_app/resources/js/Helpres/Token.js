f6e1e78457e5c74018a51f8e7ba33627cf612dfb
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-07-24 14:11:05 +0100

    yep

--- ecom_app/resources/js/Helpres/Token.js
+++ ecom_app/resources/js/Helpres/Token.js
@@ -22,15 +22,6 @@ class Token{
         return JSON.parse(atob(payload))
     }
 
-    // parseJwt (token) {
-    //     let base64Url = token.split('.')[1];
-    //     let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    //     let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    //         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    //     }).join(''));
-    
-    //     return JSON.parse(jsonPayload);
-    // };
 }
 
 export default Toekn = new Token();
