f12111417481926c5bde5e324b6cec39b0b87419
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-08-04 11:27:37 +0100

    add empolyee

--- ecom_app/resources/js/components/employee/index.vue
+++ ecom_app/resources/js/components/employee/index.vue
@@ -5,27 +5,55 @@
         >Add Employee
       </router-link>
     </div>
-    <div class="row justify-content-center">
-      <div class="col-xl-12 col-lg-12 col-md-12">
-        <div class="card shadow-sm my-5">
-          <div class="card-body p-0">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="login-form">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">All Employee</h1>
-                  </div>
-
-                  <hr />
-                  <div class="text-center"></div>
-                  <div class="text-center"></div>
-                </div>
-              </div>
-            </div>
+    <br /><br />
+    <div class="row">
+      <div class="col-lg-12 mb-4">
+        <!-- Simple Tables -->
+        <div class="card">
+          <div
+            class="
+              card-header
+              py-3
+              d-flex
+              flex-row
+              align-items-center
+              justify-content-between
+            "
+          >
+            <h6 class="m-0 font-weight-bold text-primary">Employee List</h6>
           </div>
+          <div class="table-responsive">
+            <table class="table align-items-center table-flush">
+              <thead class="thead-light">
+                <tr>
+                  <th>Name</th>
+                  <th>Photo</th>
+                  <th>Phone Number</th>
+                  <th>Sallery</th>
+                  <th>Joining Dste</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="empolyee in employees" :key="empolyee.id">
+                  <td>{{ empolyee.name }}</td>
+                  <td><img :src="empolyee.photo" alt="" id="m_photo" /></td>
+                  <td>{{ empolyee.phone }}</td>
+                  <td>{{ empolyee.sallery }}</td>
+                  <td>{{ empolyee.joining_date }}</td>
+                  <td>
+                    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="card-footer"></div>
         </div>
       </div>
     </div>
+    <!--Row-->
   </div>
 </template>
 
@@ -41,11 +69,27 @@ export default {
   },
   components: {},
   data() {
-    return {};
+    return {
+      employees: [],
+    };
   },
-  methods: {},
+  methods: {
+    allEmplyee() {
+      axios
+        .get("/api/employee")
+        .then(({ data }) => (this.employees = data))
+        .catch((err) => {});
+    },
+  },
+  created() {
+    this.allEmplyee();
+  },
 };
 </script>
 
 <style scoped>
+#m_photo {
+  height: 40px;
+  width: 40px;
+}
 </style>
