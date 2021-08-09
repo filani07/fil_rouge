5edcdbaa354d10f45b89c18d1c479b5ed16aad46
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-08-09 19:05:27 +0100

    add product

--- ecom_app/resources/views/index.blade.php
+++ ecom_app/resources/views/index.blade.php
@@ -215,7 +215,21 @@
                     </div>
                     <!---Container Fluid-->
                 </div>
-
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+                    <div class="container my-auto">
+                        <div class="copyright text-center my-auto">
+                            <span>copyright &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script>
+                                - developed by
+                                <b><a href="" target="_blank">Oussama Filani</a></b>
+                            </span>
+                        </div>
+                    </div>
+                </footer>
+                <!-- Footer -->
             </div>
         </div>
     </div>
