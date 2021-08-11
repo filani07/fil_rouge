b7e4e1c5e2eb7d9ede776af0fa23a38272fc8110
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-08-11 22:56:53 +0100

    add layout components

---
+++ ecom_app/resources/js/components/layout/TopBar.vue
@@ -0,0 +1,73 @@
+
+            <div id="content-wrapper" class="d-flex flex-column">
+                <div id="content">
+                    <!-- TopBar -->
+                    <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top" id="topbar"
+                        v-show="$route.path === '/' ||  $route.path==='/register' ||  $route.path==='/forget' ? flase : true"
+                        style="display: none">
+                        <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <ul class="navbar-nav ml-auto">
+
+
+
+
+                            <div class="topbar-divider d-none d-sm-block"></div>
+                            <li class="nav-item dropdown no-arrow">
+                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img class="img-profile rounded-circle" src="{{ asset('assets/img/boy.png') }}"
+                                        style="max-width: 60px">
+                                    <router-link to="/logout" class="ml-2 d-none d-lg-inline text-white small">Logout
+                                    </router-link>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                    aria-labelledby="userDropdown">
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Profile
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Settings
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Activity Log
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="login.html">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Logout
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- Topbar -->
+
+                    <!-- Container Fluid-->
+                    <div class="container-fluid" id="container-wrapper">
+                        <router-view>
+
+
+                    </div>
+                    <!---Container Fluid-->
+                </div>
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+                    <div class="container my-auto">
+                        <div class="copyright text-center my-auto">
+                            <span>copyright &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script>
+                                - developed by
+                                <b><a href="" target="_blank">Oussama Filani</a></b>
+                            </span>
+                        </div>
+                    </div>
+                </footer>
+                <!-- Footer -->
+            </div>
