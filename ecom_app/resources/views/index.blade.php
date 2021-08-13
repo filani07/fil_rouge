7eb5039e49259f7b79d9a24e1cf800a6385d5322
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-08-13 12:12:01 +0100

    add shopping page

--- ecom_app/resources/views/index.blade.php
+++ ecom_app/resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <title>MyEcom - Dashboard</title>
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('assets/css/ruang-admin.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/ruang-admin.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
