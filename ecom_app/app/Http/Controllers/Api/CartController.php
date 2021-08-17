383d281f2d0e3baa1a7f742d6c5025ecc49973de
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-08-18 00:37:43 +0100

    edit admin dashboard

--- ecom_app/app/Http/Controllers/Api/CartController.php
+++ ecom_app/app/Http/Controllers/Api/CartController.php
@@ -68,27 +68,6 @@ class CartController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-    }
 
     /**
      * Remove the specified resource from storage.
--- ecom_app/app/Http/Controllers/Api/OrderController.php
+++ ecom_app/app/Http/Controllers/Api/OrderController.php
@@ -54,39 +54,6 @@ class OrderController extends Controller
         $Order->save();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
 
     public function confirmedOrder(Request $request)
     {
